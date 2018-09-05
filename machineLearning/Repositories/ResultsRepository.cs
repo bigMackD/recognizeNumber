59520ef4fcc43d4d8edfffcd320d62c2eb89f82c
Maciek <maciek.d@me.com>
2018-09-05 10:43:04 +0200

    Added repository for data

---
+++ machineLearning/Repositories/ResultsRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace machineLearning.Repositories
+{
+    public class ResultsRepository
+    {
+        private static List<Result> _allResults = new List<Result>();
+
+        public void Add(Result result)
+        {
+            _allResults.Add(result);
+        }
+
+        public IEnumerable<Result> GetResults()
+        {
+            return _allResults;
+        }
+       
+    }
+}
