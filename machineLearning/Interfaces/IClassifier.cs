d454ef80a74c8a72f8e4cbfbd4e6a31dc2f31056
Maciek <maciek.d@me.com>
2018-09-05 10:44:42 +0200

    Added method for classifying

---
+++ machineLearning/Interfaces/IClassifier.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace machineLearning.Interfaces
+{
+    interface IClassifier
+    {
+        void Train (IEnumerable<Record> TrainingSet);
+        int Predict(int[] pixels, Record[] sampleRecords);
+    }
+
+    public class BasicClassifier : IClassifier
+    {
+        public int Predict(int[] pixels, Record[] sampleRecords)
+        {
+           foreach (var numberToValidate in pixels)
+            {
+                Distance()
+            }
+        }
+
+        public void Train(IEnumerable<Record> TrainingSet)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
