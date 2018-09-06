6bec58d41ebd89a2cf57b6680d9835e5ec88133c
Maciek <maciek.d@me.com>
2018-09-06 09:50:10 +0200

    fixed tests for distance method

--- Methods.Tests/UnitTest1.cs
+++ Methods.Tests/UnitTest1.cs
@@ -1,6 +1,7 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using machineLearning.Interfaces;
 using machineLearning;
+using System.Diagnostics;
 
 namespace Methods.Tests
 {
@@ -14,19 +15,26 @@ namespace Methods.Tests
             //arrange
             Record record = new Record();
             record.Number = 1;
-            int[] a = { 1, 4 };
+            int[] a = {1, 1, 4 };
             record.Pixels = a;
 
             Record record1 = new Record();
             record.Number = 1;
-            int[] b = { 3, 4 };
-            record.Pixels = b;
+            int[] b = {1, 3, 4 };
+            record1.Pixels = b;
+
+            Result expectedResult = new Result();
+            expectedResult.Distance = 2;
+            expectedResult.Number = 1;
+
 
             Distance distance = new Distance();
             //act
             var result = distance.Between(record,record1);
             //assert
-            Assert.AreEqual(0, result);
+            Debug.WriteLine(result);
+            System.Console.WriteLine(result);
+            Assert.AreEqual(expectedResult.Distance, result.Distance);
         }
     }
 }
