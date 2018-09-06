c8df1223677143d0860f421e7aa3a3945217c378
Maciek <maciek.d@me.com>
2018-09-06 12:15:06 +0200

    added functionality that lets choose image to identify

--- machineLearning/InputHelpers/ReadHelper.cs
+++ machineLearning/InputHelpers/ReadHelper.cs
@@ -1,10 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using System.Text;
 
 namespace machineLearning.InputHelpers
 {
-    class ReadHelper
+    public class ReadHelper
     {
+        public Record[] Records { get; set; }
+
+        public ReadHelper(string path)
+        {
+            string[] data = File.ReadAllLines(path);
+            var rawData = data.Skip(1);
+            var rawDataSplitted = rawData.Select(x => x.Split(',')).ToArray();
+            var records = rawDataSplitted.Select(x =>
+            new Record
+            {
+                Number = int.Parse(x.First()),
+                Pixels = (x.Skip(1).Select(n => int.Parse(n)).ToArray())
+            }
+            );
+            var recordsArray = records.ToArray();
+            records = recordsArray;
+            this.Records = recordsArray;
+        }
     }
 }
--- machineLearning/Program.cs
+++ machineLearning/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using machineLearning.Interfaces;
+using machineLearning.InputHelpers;
 
 namespace machineLearning
 {
@@ -13,37 +14,23 @@ namespace machineLearning
         {
             IClassifier classifier = new BasicClassifier();
 
-            string[] data = File.ReadAllLines("trainingsample.csv");
-            var rawData = data.Skip(1);
-            var rawDataSplitted = rawData.Select(x => x.Split(',')).ToArray();
-            var records = rawDataSplitted.Select(x =>
-            new Record
-            {
-                Number = int.Parse(x.First()),
-                Pixels = (x.Skip(1).Select(n => int.Parse(n)).ToArray())
-            }
-            );
-            var recordsArray = records.ToArray();
+            ReadHelper trainingSamples = new ReadHelper("trainingsample.csv");
+            var trainingSample = trainingSamples.Records;
             Console.WriteLine("Training done");
-            
-            
-            string[] dataToValidate = File.ReadAllLines("validationsample.csv");
-            var rawDataToValidate = dataToValidate.Skip(1);
-            var rawDataSplittedToValidate = rawDataToValidate.Select(x => x.Split(',')).ToArray();
-            var recordsToValidate = rawDataSplittedToValidate.Select(x =>
-            new Record
-            {
-                Number = int.Parse(x.First()),
-                Pixels = (x.Skip(1).Select(n => int.Parse(n)).ToArray())
-            }
-            );
-            Console.WriteLine("Validate done");
-            var recordToValidate = recordsToValidate.Last();
 
-            var results =  classifier.Predict(recordToValidate, recordsArray).ToArray();
-            var sortedResult = results.OrderBy(x => x.Distance).First(); 
-            Console.WriteLine($"Number:{sortedResult.Number}, distance: {sortedResult.Distance}");
+            Console.WriteLine("Which image(as row of file) would you like to identify?");
+            var k = int.Parse(Console.ReadLine());
+
+            ReadHelper validationSamples = new ReadHelper("validationsample.csv");
+            var recordToValidate = validationSamples.Records[k];
+            Console.WriteLine("Validate done");
+
+            var result =  classifier.Predict(recordToValidate, trainingSamples.Records).ToArray();
+            var sortedResult = result.OrderBy(x => x.Distance).First(); 
+            Console.WriteLine($"Probably it is {sortedResult.Number}, it's distance is {Math.Floor(sortedResult.Distance)}");
             Console.ReadLine();
+
+           // var results = validationSamples.Records.Select(x => classifier.Predict(x, trainingSamples.Records)).ToArray();
 
         }
       
